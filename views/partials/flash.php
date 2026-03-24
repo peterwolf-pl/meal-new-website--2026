da5d0cc5285496075746b3b77391e58d7a5519fc
Peter Wolf <84736182+peterwolf-pl@users.noreply.github.com>
2026-03-24 18:32:29 +0100

    Delete views directory

--- views/partials/flash.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($flashMessages)): ?>
-    <div class="flash-stack" role="status" aria-live="polite">
-        <?php foreach ($flashMessages as $message): ?>
-            <div class="flash flash-<?= e($message['type']) ?>">
-                <p><?= e($message['message']) ?></p>
-            </div>
-        <?php endforeach; ?>
-    </div>
-<?php endif; ?>
